194eccc74905bc0835f950959cac671825c1e0ed
YusufTube <hoseinpro1391@gmail.com>
2026-02-26 22:36:20 +0300

    Update RegisterScript.js

--- JavaScript/RegisterScript.js
+++ JavaScript/RegisterScript.js
@@ -1,8 +1,22 @@
-const togglePassword = document.getElementById("togglePassword");
-const password = document.getElementById("password");
+document.addEventListener("DOMContentLoaded", function () {
 
-togglePassword.addEventListener("click", function () {
-    password.type = password.type === "password" ? "text" : "password";
-    this.classList.toggle("fa-eye");
-    this.classList.toggle("fa-eye-slash");
+    const togglePassword = document.getElementById("togglePassword");
+    const password = document.getElementById("password");
+
+    if (togglePassword && password) {
+        togglePassword.addEventListener("click", function () {
+
+            if (password.type === "password") {
+                password.type = "text";
+                this.classList.remove("fa-eye");
+                this.classList.add("fa-eye-slash");
+            } else {
+                password.type = "password";
+                this.classList.remove("fa-eye-slash");
+                this.classList.add("fa-eye");
+            }
+
+        });
+    }
+
 });
