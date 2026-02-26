2bb7624baef94b77b1e4b5419771b7342c64a419
YusufTube <hoseinpro1391@gmail.com>
2026-02-26 19:56:41 +0300

    Update RegisterScript.js

--- JavaScript/RegisterScript.js
+++ JavaScript/RegisterScript.js
@@ -1,21 +1,18 @@
+/* PASSWORD TOGGLE */
 document.addEventListener("DOMContentLoaded", function () {
 
+    const togglePassword = document.getElementById("togglePassword");
     const passwordInput = document.getElementById("passwordInput");
-    const togglePasswordBtn = document.getElementById("togglePasswordBtn");
-    const eyeIcon = togglePasswordBtn.querySelector("i");
 
-    togglePasswordBtn.addEventListener("click", function () {
+    if (togglePassword && passwordInput) {
+        togglePassword.addEventListener("click", function () {
 
-        if (passwordInput.type === "password") {
-            passwordInput.type = "text";
-            eyeIcon.classList.remove("fa-eye");
-            eyeIcon.classList.add("fa-eye-slash");
-        } else {
-            passwordInput.type = "password";
-            eyeIcon.classList.remove("fa-eye-slash");
-            eyeIcon.classList.add("fa-eye");
-        }
+            const type = passwordInput.type === "password" ? "text" : "password";
+            passwordInput.type = type;
 
-    });
+            this.classList.toggle("fa-eye");
+            this.classList.toggle("fa-eye-slash");
+        });
+    }
 
 });
